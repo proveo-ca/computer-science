26744dbc82a7df8d696709ebf057d7bc1691b24a
rschoettler@proveo.ca <rschoettler@proveo.ca>
2025-07-23 20:38:08 +0000

    refactor: update import paths to use module aliases

--- libs/datatypes/index.ts
+++ libs/datatypes/index.ts
@@ -1,33 +1,33 @@
 // Node implementations
-export { BinaryTreeNode } from './binary-tree-node/binary-tree-node';
-export { DoublyLinkedListNode } from './doubly-linked-list-node/doubly-linked-list-node';
-export { LinkedListNode } from './linked-list-node/linked-list-node';
-export { TrieNode } from './trie-node/trie-node';
+export { BinaryTreeNode } from '@/binary-tree-node';
+export { DoublyLinkedListNode } from '@/doubly-linked-list-node';
+export { LinkedListNode } from '@/linked-list-node';
+export { TrieNode } from '@/trie-node';
 
 // Linear data structures
-export { LinkedList } from './linked-list/linked-list';
-export { DoublyLinkedList } from './doubly-linked-list/doubly-linked-list';
-export { Stack } from './stack/stack';
-export { Queue } from './queue/queue';
-export { Deque } from './deque/deque';
+export { LinkedList } from '@/linked-list';
+export { DoublyLinkedList } from '@/doubly-linked-list';
+export { Stack } from '@/stack';
+export { Queue } from '@/queue';
+export { Deque } from '@/deque';
 
 // Tree data structures
-export { BinaryTree } from './binary-tree/binary-tree';
-export { Trie } from './trie/trie';
+export { BinaryTree } from '@/binary-tree';
+export { Trie } from '@/trie';
 
 // Heap data structures
-export { Heap } from './heap/heap';
-export { MinHeap } from './min-heap/min-heap';
-export { MaxHeap } from './max-heap/max-heap';
+export { Heap } from '@/heap';
+export { MinHeap } from '@/min-heap';
+export { MaxHeap } from '@/max-heap';
 
 // Priority Queue data structures
-export { PriorityQueue, PriorityNode } from './priority-queue/priority-queue';
-export { MinPriorityQueue } from './min-priority-queue/min-priority-queue';
-export { MaxPriorityQueue } from './max-priority-queue/max-priority-queue';
+export { PriorityQueue, PriorityNode } from '@/priority-queue';
+export { MinPriorityQueue } from '@/min-priority-queue';
+export { MaxPriorityQueue } from '@/max-priority-queue';
 
 // Graph data structures
-export { Graph } from './graph/graph';
-export { DirectedGraph } from './directed-graph/directed-graph';
+export { Graph } from '@/graph';
+export { DirectedGraph } from '@/directed-graph';
 
 // Legacy exports (if needed)
-export { GeneratorLinkedList } from './generator-linked-list/generator-linked-list';
+export { GeneratorLinkedList } from '@/generator-linked-list';
