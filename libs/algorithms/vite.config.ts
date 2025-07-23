b4e3a9e4a5f2a4e5978b98f76a08175fc729aeab
rschoettler@proveo.ca <rschoettler@proveo.ca>
2025-07-23 21:56:09 +0000

    refactor: improve Vite config type handling and formatting

--- libs/algorithms/vite.config.ts
+++ libs/algorithms/vite.config.ts
@@ -7,21 +7,23 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Algorithms',
-      fileName: 'algorithms'
+      fileName: 'algorithms',
     },
     rollupOptions: {
       input: Object.fromEntries(
-        glob.sync('src/**/*.ts', {
-          ignore: ['src/**/*.spec.ts', 'src/**/*.test.ts']
-        }).map(file => [
-          // This remove `src/` as well as the file extension from each file, so e.g.
-          // src/nested/foo.ts becomes nested/foo
-          file.slice(4, file.length - 3),
-          // This expands the relative paths to absolute paths, so e.g.
-          // src/nested/foo.ts becomes /project/src/nested/foo.ts
-          resolve(__dirname, file)
-        ])
-      )
-    }
-  }
-})
+        glob
+          .sync('src/**/*.ts', {
+            ignore: ['src/**/*.spec.ts', 'src/**/*.test.ts'],
+          })
+          .map((file: string | any[]) => [
+            // This remove `src/` as well as the file extension from each file, so e.g.
+            // src/nested/foo.ts becomes nested/foo
+            file.slice(4, file.length - 3),
+            // This expands the relative paths to absolute paths, so e.g.
+            // src/nested/foo.ts becomes /project/src/nested/foo.ts
+            resolve(__dirname, <string>file),
+          ])
+      ),
+    },
+  },
+});
